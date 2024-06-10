5a8e87f8a64ee783673f7918189bfebcb3991cbe
SANTHOSHKUMAR S <114432961+comdo-santhosh@users.noreply.github.com>
2024-06-10 10:28:38 +0530

    feat : added Core Java Files

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+// Writing helloworld.java
+// Simple Program
+public class helloworld {
+    public static void main(String[]args){
+        System.out.println("Helloworld");
+        System.out.print("Name : ");
+        System.out.println("Velanyutham ");
+        System.out.print("Hello");  // printl() -- start with New line
+        System.out.println("world");
+        System.out.printf("helloworld %.2f",123f);
+    }
+}
